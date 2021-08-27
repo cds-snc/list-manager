c1ac830060e4e30d78829f7fcb3882afa659fb8d
Pat Heard <patrick.heard@cds-snc.ca>
2021-08-27 15:59:52 -0400

    feat: enable VPC flow logs (#9)
    This will allow us to diagnose the Lambda internet connectivity issues.
    
    Also adds the 443 egress rules to the security group used by the Lambda.

--- terragrunt/aws/api/inputs.tf
+++ terragrunt/aws/api/inputs.tf
@@ -3,12 +3,15 @@ variable "notify_key" {
   sensitive = true
 }
 
-
 variable "rds_password" {
   type      = string
   sensitive = true
 }
 
 variable "rds_username" {
+  type = string
+}
+
+variable "rds_proxy_security_group_id" {
   type = string
 }
--- terragrunt/aws/api/vpc.tf
+++ terragrunt/aws/api/vpc.tf
@@ -3,5 +3,27 @@ module "vpc" {
   name              = var.product_name
   billing_tag_value = var.billing_code
   high_availability = true
-  enable_flow_log   = false
+  enable_flow_log   = true
+}
+
+data "aws_security_group" "rds_proxy_sg" {
+  id = var.rds_proxy_security_group_id
+}
+
+resource "aws_security_group_rule" "rds_sg_egress_443" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = data.aws_security_group.rds_proxy_sg.id
+}
+
+resource "aws_security_group_rule" "rds_sg_egress_5432" {
+  type              = "egress"
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  security_group_id = data.aws_security_group.rds_proxy_sg.id
+  self              = true
 }
