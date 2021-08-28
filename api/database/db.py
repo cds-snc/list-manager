522a0a1825e6e38c1ed43b204dcada11f139e827
Max Neuvians <max.neuvians@cds-snc.ca>
2021-08-28 23:41:10 +0000

    fix: set the functions to sync

--- api/database/db.py
+++ api/database/db.py
@@ -1,17 +1,12 @@
 import os
 
-from sqlalchemy import create_engine, engine
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if os.environ.get("CI"):
     connection_string = os.environ.get("SQLALCHEMY_DATABASE_TEST_URI")
 else:
     connection_string = os.environ.get("SQLALCHEMY_DATABASE_URI")
 # Timeout is set to 10 seconds
-
-
-def get_engine() -> engine.Engine:
-    return create_engine(
-        connection_string,
-        connect_args={"connect_timeout": 10},
-        pool_pre_ping=True,
-    )
+db_engine = create_engine(connection_string, connect_args={"connect_timeout": 10})
+db_session = sessionmaker(bind=db_engine)
