c400c8d9212262257c34a6bed69d6a5f191086e1
Max Neuvians <max.neuvians@cds-snc.ca>
2021-09-09 15:28:54 +0000

    feat: add 502

--- terragrunt/aws/api/cloudwatch_log.tf
+++ terragrunt/aws/api/cloudwatch_log.tf
@@ -13,6 +13,18 @@ resource "aws_cloudwatch_log_metric_filter" "lambda-500-errors" {
   }
 }
 
+resource "aws_cloudwatch_log_metric_filter" "lambda-502-errors" {
+  name           = "502-errors"
+  pattern        = "\"\\\" 502 \""
+  log_group_name = "aws/lambda/${aws_lambda_function.api.function_name}"
+
+  metric_transformation {
+    name      = "502-errors"
+    namespace = "LogMetrics"
+    value     = "1"
+  }
+}
+
 resource "aws_cloudwatch_log_metric_filter" "lambda-429-errors" {
   name           = "429-errors"
   pattern        = "\"\\\" 429 \""
