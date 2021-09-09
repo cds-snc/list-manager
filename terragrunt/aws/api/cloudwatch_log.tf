04fb194c7c6cbd353acaf5f32a5810a2eaab744b
Max Neuvians <max.neuvians@cds-snc.ca>
2021-09-09 15:06:00 +0000

    feat: add metrics on 500 and 429 errors on logs

---
+++ terragrunt/aws/api/cloudwatch_log.tf
@@ -0,0 +1,26 @@
+###
+# AWS log log metric filters
+###
+resource "aws_cloudwatch_log_metric_filter" "500-errors" {
+  name           = "500-errors"
+  pattern        = "\"\\\" 500 \""
+  log_group_name = "aws/lambda/${aws_lambda_function.api.function_name}"
+
+  metric_transformation {
+    name      = "500-errors"
+    namespace = "LogMetrics"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "429-errors" {
+  name           = "429-errors"
+  pattern        = "\"\\\" 429 \""
+  log_group_name = "aws/lambda/${aws_lambda_function.api.function_name}"
+
+  metric_transformation {
+    name      = "429-errors"
+    namespace = "LogMetrics"
+    value     = "1"
+  }
+}
