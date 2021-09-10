454a61340d71ccc4307be2e3d67c912d4ef47997
Max Neuvians <max@neuvians.net>
2021-09-10 09:30:21 -0400

    fix: use API gateway codes vs lambda codes (#62)
    * fix: use API gateway codes vs lambda codes
    
    * fix: dup attributes
    
    * fix: lint

--- api/main.py
+++ api/main.py
@@ -3,21 +3,13 @@ from api_gateway import api
 from logger import log
 from database.migrate import migrate_head
 from aws_lambda_powertools import Metrics
-import os
 
 app = api.app
 metrics = Metrics(namespace="ListManager", service="api")
 
 
-def print_env_variables():
-    rapi = os.getenv("AWS_LAMBDA_RUNTIME_API", "NOT_FOUND")
-    log.info(f"AWS_LAMBDA_RUNTIME_API: {rapi}")
-
-
 @metrics.log_metrics(capture_cold_start_metric=True)
 def handler(event, context):
-    print_env_variables()
-    log.debug(event)
     if "httpMethod" in event:
         # Assume it is an API Gateway event
         asgi_handler = Mangum(app)
