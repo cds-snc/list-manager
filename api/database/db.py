97f443440a1b370cf68e4524db2573e61e2802a8
Pat Heard <patrick.heard@cds-snc.ca>
2021-09-27 10:06:48 -0400

    fix: SQLAlchemy connection pool settings (#92)
    These connection pool settings do the following:
    1. Test that a connection is still active before use.
    2. Set a connection to expire sooner than the RDS Proxy timeout.
    3. Always re-use the most recently used connection.

--- api/database/db.py
+++ api/database/db.py
@@ -12,5 +12,8 @@ db_engine = create_engine(
     connection_string,
     pool_size=1,
     max_overflow=0,
+    pool_pre_ping=True,  # Check that a connection is still active before attempting to use
+    pool_recycle=1500,  # Prune connections older than 25 minutes (RDS Proxy has a timeout of 30 minutes)
+    pool_use_lifo=True,  # Always re-use last connection used (allows server-side timeouts to remove unused connections)
 )
 db_session = sessionmaker(bind=db_engine)
