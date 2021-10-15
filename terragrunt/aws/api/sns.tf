0c2281587bd1ea8bcad0617febb2e80c228a72d0
Pat Heard <patrick.heard@cds-snc.ca>
2021-10-15 18:56:34 +0000

    feat: switch to Notify Slack Terraform module
    Use the common CDS module for Slack notification from CloudWatch Alarms.

--- terragrunt/aws/api/sns.tf
+++ terragrunt/aws/api/sns.tf
@@ -33,13 +33,13 @@ resource "aws_sns_topic" "warning_us_east" {
 resource "aws_sns_topic_subscription" "critical" {
   topic_arn = aws_sns_topic.critical.arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.notify_slack.arn
+  endpoint  = module.notify_slack.lambda_arn
 }
 
 resource "aws_sns_topic_subscription" "warning" {
   topic_arn = aws_sns_topic.warning.arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.notify_slack.arn
+  endpoint  = module.notify_slack.lambda_arn
 }
 
 resource "aws_sns_topic_subscription" "warning_us_east" {
@@ -47,142 +47,28 @@ resource "aws_sns_topic_subscription" "warning_us_east" {
 
   topic_arn = aws_sns_topic.warning_us_east.arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.notify_slack.arn
+  endpoint  = module.notify_slack.lambda_arn
 }
 
 #
 # Lambda: post notifications to Slack
 #
-resource "aws_lambda_function" "notify_slack" {
-  # checkov:skip=CKV_AWS_50: X-ray tracing not required for this function
-  # checkov:skip=CKV_AWS_115: No function-level concurrent execution limit required
-  # checkov:skip=CKV_AWS_116: No Dead Letter Queue required
-  # checkov:skip=CKV_AWS_173: Lambda environment variable encryption with default KMS key is acceptable
+#
+# Lambda: post notifications to Slack
+#
+module "notify_slack" {
+  source = "github.com/cds-snc/terraform-modules?ref=v0.0.36//notify_slack"
 
-  function_name = "notify_slack"
-  description   = "Lambda function to post CloudWatch alarm notifications to a Slack channel."
+  function_name     = "notify_slack"
+  project_name      = "ListManager"
+  slack_webhook_url = var.slack_webhook_url
 
-  filename    = data.archive_file.notify_slack.output_path
-  handler     = "notify_slack.lambda_handler"
-  runtime     = "python3.8"
-  timeout     = 30
-  memory_size = 1024
-
-  role             = aws_iam_role.notify_slack_lambda.arn
-  source_code_hash = filebase64sha256(data.archive_file.notify_slack.output_path)
-
-  environment {
-    variables = {
-      SLACK_WEBHOOK_URL = var.slack_webhook_url
-      PROJECT_NAME      = "ListManager"
-      LOG_EVENTS        = "True"
-    }
-  }
-
-  vpc_config {
-    security_group_ids = [aws_security_group.notify_slack.id]
-    subnet_ids         = module.vpc.private_subnet_ids
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.notify_slack_lambda,
-    aws_cloudwatch_log_group.notify_slack_lambda,
+  sns_topic_arns = [
+    aws_sns_topic.warning.arn,
+    aws_sns_topic.warning_us_east.arn,
+    aws_sns_topic.critical.arn
   ]
 
-  tags = {
-    CostCenter = var.billing_code
-  }
-}
-
-data "archive_file" "notify_slack" {
-  type        = "zip"
-  source_file = "notify_slack/notify_slack.py"
-  output_path = "/tmp/notify_slack.py.zip"
-}
-
-resource "aws_lambda_permission" "notify_slack_warning" {
-  statement_id  = "AllowExecutionFromSNSWarning"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.notify_slack.function_name
-  principal     = "sns.amazonaws.com"
-  source_arn    = aws_sns_topic.warning.arn
-}
-
-resource "aws_lambda_permission" "notify_slack_warning_us_east" {
-  statement_id  = "AllowExecutionFromSNSWarningUsEast"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.notify_slack.function_name
-  principal     = "sns.amazonaws.com"
-  source_arn    = aws_sns_topic.warning_us_east.arn
-}
-
-resource "aws_lambda_permission" "notify_slack_critical" {
-  statement_id  = "AllowExecutionFromSNSCritical"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.notify_slack.function_name
-  principal     = "sns.amazonaws.com"
-  source_arn    = aws_sns_topic.critical.arn
-}
-
-#
-# IAM: Lambda logs
-#
-resource "aws_iam_role" "notify_slack_lambda" {
-  name               = "NotifySlackLambda"
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_policy.json
-}
-
-resource "aws_iam_policy" "notify_slack_lambda" {
-  name   = "NotifySlackLambda"
-  path   = "/"
-  policy = data.aws_iam_policy_document.notify_slack_lambda.json
-}
-
-resource "aws_iam_role_policy_attachment" "notify_slack_lambda" {
-  role       = aws_iam_role.notify_slack_lambda.name
-  policy_arn = aws_iam_policy.notify_slack_lambda.arn
-}
-
-resource "aws_iam_role_policy_attachment" "notify_slack_lambda_vpc" {
-  role       = aws_iam_role.notify_slack_lambda.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-}
-
-data "aws_iam_policy_document" "lambda_assume_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "sts:AssumeRole",
-    ]
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "notify_slack_lambda" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    resources = [
-      aws_cloudwatch_log_group.notify_slack_lambda.arn
-    ]
-  }
-}
-
-#
-# CloudWatch: Lambda logs
-#
-resource "aws_cloudwatch_log_group" "notify_slack_lambda" {
-  # checkov:skip=CKV_AWS_158: CloudWatch log group encryption with default KMS key is acceptable
-  name              = "/aws/lambda/notify_slack"
-  retention_in_days = "14"
-
-  tags = {
-    CostCenter = var.billing_code
-  }
+  billing_tag_key   = "CostCenter"
+  billing_tag_value = var.billing_code
 }
