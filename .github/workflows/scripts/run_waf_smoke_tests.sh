9aec6e90ecc44040cfeb89105aafa0cb62cb3b62
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-01-07 12:34:04 -0500

    chore: provide which path was blocked if smoketest fails

--- .github/workflows/scripts/run_waf_smoke_tests.sh
+++ .github/workflows/scripts/run_waf_smoke_tests.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 input=".github/workflows/scripts/inputs/valid_paths.txt"
+blocks=0
 while IFS= read -r path
 do
   response=$(curl --write-out '%{http_code}' --silent --output /dev/null "$path")
   if [[ $response -eq 204 ]]; then
-    exit 1 # Error 204 indicates that the request was blocked by the AWS WAF
+    echo "$path blocked by the AWS WAF"
+    ((blocks++)) # Error 204 indicates that the request was blocked by the AWS WAF
   fi
 done < "$input"
+
+if [[ $blocks -gt 0 ]]; then
+  exit 1
+fi
