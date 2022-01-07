3a6a5aa9d52d779e9bb7054c30c07a53211de803
Mohamed Nur <85885638+mohamed-cds@users.noreply.github.com>
2022-01-07 12:57:36 -0500

    feat: migrate waf to allowlist (#101)
    * feat: migrate waf to allowlist
    
    * chore: merge regex to get below 10 regex limit
    
    * chore: minor formatting
    
    * feat: WAF smoketests
    
    * chore: provide which path was blocked if smoketest fails

--- terragrunt/aws/api/waf.tf
+++ terragrunt/aws/api/waf.tf
@@ -30,8 +30,70 @@ resource "aws_wafv2_web_acl" "api_waf" {
   }
 
   rule {
-    name     = "AWSManagedRulesCommonRuleSet"
+    name     = "api_rate_limit"
     priority = 2
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 10000
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "api_rate_limit"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "api_invalid_path"
+    priority = 10
+
+    action {
+      block {
+        custom_response {
+          response_code = 204
+        }
+      }
+    }
+
+    statement {
+      not_statement {
+        statement {
+          regex_pattern_set_reference_statement {
+            arn = aws_wafv2_regex_pattern_set.re_list_manager_api.arn
+            field_to_match {
+              uri_path {}
+            }
+            text_transformation {
+              priority = 1
+              type     = "COMPRESS_WHITE_SPACE"
+            }
+            text_transformation {
+              priority = 2
+              type     = "LOWERCASE"
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "api_invalid_path"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 20
 
     override_action {
       none {}
@@ -57,7 +119,7 @@ resource "aws_wafv2_web_acl" "api_waf" {
 
   rule {
     name     = "AWSManagedRulesKnownBadInputsRuleSet"
-    priority = 3
+    priority = 30
 
     override_action {
       none {}
@@ -79,7 +141,7 @@ resource "aws_wafv2_web_acl" "api_waf" {
 
   rule {
     name     = "AWSManagedRulesLinuxRuleSet"
-    priority = 4
+    priority = 40
 
     override_action {
       none {}
@@ -101,7 +163,7 @@ resource "aws_wafv2_web_acl" "api_waf" {
 
   rule {
     name     = "AWSManagedRulesSQLiRuleSet"
-    priority = 5
+    priority = 50
 
     override_action {
       none {}
@@ -121,28 +183,6 @@ resource "aws_wafv2_web_acl" "api_waf" {
     }
   }
 
-  rule {
-    name     = "api_rate_limit"
-    priority = 101
-
-    action {
-      block {}
-    }
-
-    statement {
-      rate_based_statement {
-        limit              = 10000
-        aggregate_key_type = "IP"
-      }
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "api_rate_limit"
-      sampled_requests_enabled   = true
-    }
-  }
-
   visibility_config {
     cloudwatch_metrics_enabled = true
     metric_name                = "api"
@@ -150,6 +190,49 @@ resource "aws_wafv2_web_acl" "api_waf" {
   }
 }
 
+resource "aws_wafv2_regex_pattern_set" "re_list_manager_api" {
+  name        = "re_list_manager_api"
+  description = "Regex matching valid list manager endpoints"
+  scope       = "REGIONAL"
+
+  # WAF Regex blocks are combined with OR logic. 
+  # Regex support is limited, please see: 
+  # https://docs.aws.amazon.com/waf/latest/developerguide/waf-regex-pattern-set-managing.html
+
+  # GET /healthcheck
+  # POST /list
+  # GET /lists
+  # POST /send
+  # POST /subscription
+  # GET /version
+  regular_expression {
+    regex_string = "/healthcheck|/list|/lists|/send|/subscription|/version"
+  }
+
+  # PUT /list/<uuid:list_id>/reset
+  # GET /lists/<uuid:service_id>
+  # PUT /lists/<uuid:service_id>
+  # GET /lists/<uuid:service_id>/subscriber-count/
+  # DELETE /lists/<uuid:service_id>
+  regular_expression {
+    regex_string = "/lists?/[\\w]{8}-[\\w]{4}-[\\w]{4}-[\\w]{4}-[\\w]{12}(/subscriber-count/|/reset)?"
+  }
+
+  # GET /subscription/<uuid:subscription_id>/confirm
+  # DELETE /subscription/<uuid:subscription_id>
+  regular_expression {
+    regex_string = "/subscription/[\\w]{8}-[\\w]{4}-[\\w]{4}-[\\w]{4}-[\\w]{12}(/confirm)?"
+  }
+
+  # GET /unsubscribe/<uuid:subscription_id>
+  regular_expression {
+    regex_string = "/unsubscribe/[\\w]{8}-[\\w]{4}-[\\w]{4}-[\\w]{4}-[\\w]{12}"
+  }
+
+  tags = {
+    CostCenter = var.billing_code
+  }
+}
 
 resource "aws_wafv2_web_acl_association" "waf_association" {
   resource_arn = aws_api_gateway_stage.api.arn
