29968ecf3995f14933dfb49ea8f71fca0ad07d35
Pat Heard <patrick.heard@cds-snc.ca>
2022-01-27 22:00:49 +0000

    fix: escape $ character in log format

--- terragrunt/aws/api/vpc.tf
+++ terragrunt/aws/api/vpc.tf
@@ -76,7 +76,7 @@ resource "aws_flow_log" "cloud_based_sensor" {
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = module.vpc.vpc_id
-  log_format           = "${vpc-id} ${version} ${account-id} ${interface-id} ${srcaddr} ${dstaddr} ${srcport} ${dstport} ${protocol} ${packets} ${bytes} ${start} ${end} ${action} ${log-status} ${subnet-id} ${instance-id}"
+  log_format           = "$${vpc-id} $${version} $${account-id} $${interface-id} $${srcaddr} $${dstaddr} $${srcport} $${dstport} $${protocol} $${packets} $${bytes} $${start} $${end} $${action} $${log-status} $${subnet-id} $${instance-id}"
 
   tags = {
     CostCentre = var.billing_code
