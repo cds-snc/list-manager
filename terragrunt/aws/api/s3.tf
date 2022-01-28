9168d78287f027e501b8f6dc410f7d391686d7cd
Pat Heard <patrick.heard@cds-snc.ca>
2022-01-28 21:12:49 +0000

    chore: remove the unused access log bucket

--- terragrunt/aws/api/s3.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  name_prefix = "${var.product_name}-${var.env}"
-}
-
-module "log_bucket" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.28//S3_log_bucket"
-  bucket_name       = "${var.product_name}-${var.env}-logs"
-  billing_tag_value = var.billing_code
-}
