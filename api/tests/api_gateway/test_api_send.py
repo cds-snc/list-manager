5f2e704f341a4b5af255ecb75daca9eefc8977e1
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-04-11 12:24:40 -0400

    broken test

--- api/tests/api_gateway/test_api_send.py
+++ api/tests/api_gateway/test_api_send.py
@@ -1,3 +1,4 @@
+import json
 import uuid
 
 from unittest.mock import patch
@@ -31,6 +32,56 @@ def test_send_email(mock_client, list_fixture, client, session):
     session.expire_all()
 
     mock_client().send_bulk_notifications.assert_called_once()
+
+
+@patch("api_gateway.api.get_notify_client")
+def test_send_email_with_personalisation(mock_client, list_fixture, client, session):
+    subscription0 = Subscription(
+        email="fixture_email_101", list=list_fixture, confirmed=True
+    )
+    session.add(subscription0)
+    session.commit()
+
+    template_id = str(uuid.uuid4())
+    response = client.post(
+        "/send",
+        json={
+            "service_api_key": str(uuid.uuid4()),
+            "list_id": str(list_fixture.id),
+            "template_id": template_id,
+            "template_type": "email",
+            "job_name": "Job Name",
+            "personalisation": json.dumps(
+                {
+                    "subject": "Subject for the email",
+                    "message": "Message of the email",
+                }
+            ),
+        },
+    )
+
+    data = response.json()
+    assert response.status_code == 200
+    assert data["status"] == "OK"
+    assert data["sent"] == 1
+    session.expire_all()
+
+    job_name = "Job Name"
+    subscribers = [
+        ["email address", "unsubscribe_link", "subject", "message"],
+        [
+            "fixture_email_101",
+            f"https://list-manager.alpha.canada.ca/unsubscribe/{list_fixture.unsubscribe_email_template_id}",
+            "Subject for the email",
+            "Message of the email",
+        ],
+    ]
+    mock_client().send_bulk_notifications.assert_called_once()
+    mock_client().send_bulk_notifications.assert_called_once_with(
+        job_name,
+        subscribers,
+        template_id,
+    )
 
 
 @patch("api_gateway.api.get_notify_client")
