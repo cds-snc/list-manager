7269534a03a80f986200032c450e7075476025b1
Pat Heard <patrick.heard@cds-snc.ca>
2023-06-20 13:11:39 +0000

    fix: pin to AWS provider v4.x

--- terragrunt/env/common/provider.tf
+++ terragrunt/env/common/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67"
+    }
+  }
+}
+
 provider "aws" {
   region              = var.region
   allowed_account_ids = [var.account_id]
