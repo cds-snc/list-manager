fb28ec460aff3cafb6a1048fef3ad609dcad4de1
Pat Heard <patrick.heard@cds-snc.ca>
2023-07-10 16:47:48 -0400

    fix: Terraform module reference (#210)
    Update the version reference for the Terraform modules
    to be after the module sub-directory.

--- terragrunt/aws/api/sns.tf
+++ terragrunt/aws/api/sns.tf
@@ -57,7 +57,7 @@ resource "aws_sns_topic_subscription" "warning_us_east" {
 # Lambda: post notifications to Slack
 #
 module "notify_slack" {
-  source = "github.com/cds-snc/terraform-modules?ref=v0.0.49//notify_slack"
+  source = "github.com/cds-snc/terraform-modules//notify_slack?ref=v0.0.49"
 
   function_name     = "notify_slack"
   project_name      = "ListManager"
