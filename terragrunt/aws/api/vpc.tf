d77b5a74ecc2c843b46d8ae87c4814f259984233
Pat Heard <patrick.heard@cds-snc.ca>
2023-07-10 16:38:13 -0400

    fix: Terraform module reference
    Update the version reference for the Terraform modules
    to be after the module sub-directory.

--- terragrunt/aws/api/vpc.tf
+++ terragrunt/aws/api/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.49//vpc"
+  source            = "github.com/cds-snc/terraform-modules//vpc?ref=v0.0.49"
   name              = var.product_name
   billing_tag_value = var.billing_code
   high_availability = true
