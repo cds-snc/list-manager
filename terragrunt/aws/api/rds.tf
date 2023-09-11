ca377bd91a062bb59c42b08529f9f0402ed7969b
Pat Heard <patrick.heard@cds-snc.ca>
2023-09-11 14:39:46 -0400

    chore: upgrade to Postgres 12.16 (#238)
    Upgrade the Postgres engine version to 12.16 which is
    the highest 12.x target allowed by the current engine version.

--- terragrunt/aws/api/rds.tf
+++ terragrunt/aws/api/rds.tf
@@ -1,14 +1,17 @@
 module "rds" {
-  source                  = "github.com/cds-snc/terraform-modules//rds?ref=v0.0.49"
-  backup_retention_period = 7
-  billing_tag_value       = var.billing_code
+  source                  = "github.com/cds-snc/terraform-modules//rds?ref=v7.0.1"
   database_name           = "list_manager"
-  instances               = 1
   name                    = "list-manager"
-  preferred_backup_window = "07:00-09:00"
-  subnet_ids              = module.vpc.private_subnet_ids
+  engine_version          = "12.16"
+  instances               = 1
   username                = var.rds_username
   password                = var.rds_password
   vpc_id                  = module.vpc.vpc_id
-  engine_version          = "11.17"
+  subnet_ids              = module.vpc.private_subnet_ids
+  backup_retention_period = 7
+  preferred_backup_window = "07:00-09:00"
+
+  allow_major_version_upgrade = true
+
+  billing_tag_value = var.billing_code
 }
