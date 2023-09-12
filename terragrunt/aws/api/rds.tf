d16ba3fac763bca99c2ffe70226c03e7beb9349d
Pat Heard <patrick.heard@cds-snc.ca>
2023-09-12 08:55:15 -0400

    chore: upgrade to Postgres 13.11
    Upgrade the Postgres database engine to 13.11.
    Additionally, change the maintenance window so
    the upgrade is applied tonight.

--- terragrunt/aws/api/rds.tf
+++ terragrunt/aws/api/rds.tf
@@ -1,17 +1,18 @@
 module "rds" {
-  source                  = "github.com/cds-snc/terraform-modules//rds?ref=v7.0.1"
+  source                  = "github.com/cds-snc/terraform-modules//rds?ref=v7.0.2"
   database_name           = "list_manager"
   name                    = "list-manager"
-  engine_version          = "12.15"
+  engine_version          = "13.11"
   instances               = 1
   username                = var.rds_username
   password                = var.rds_password
   vpc_id                  = module.vpc.vpc_id
   subnet_ids              = module.vpc.private_subnet_ids
   backup_retention_period = 7
-  preferred_backup_window = "07:00-09:00"
 
-  allow_major_version_upgrade = true
+  preferred_backup_window      = "07:00-09:00"
+  preferred_maintenance_window = "wed:06:00-wed:07:00" # timezone is UTC
+  allow_major_version_upgrade  = true
 
   billing_tag_value = var.billing_code
 }
